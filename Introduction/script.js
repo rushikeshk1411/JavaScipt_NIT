20a07aff4a50dcb7a9aa45f69c75da8db6aba119
Rushikesh Bhausaheb Khaire <rushikeshk1411@gmail.com>
2023-10-13 09:51:45 +0530

    Message

---
+++ Introduction/script.js
@@ -0,0 +1,138 @@
+/*
+console.log("Hello World");
+
+// alert("Hey I am practicing bro")
+
+console.log(prompt("Hello Is this Rushikesh"))
+
+console.log("The value of n is " +nval)
+
+var nval = 123
+
+console.log("The value of n is " +nval)
+
+function addfun(){
+    var a = 10
+    var b = 10
+    console.log(a+b)
+}
+
+console.log("Good morning here are sime steps to take care while define the function I am trying ti view how the js machine will run")
+
+prompt("Are you ready")
+
+addfun()
+*/
+
+/*
+console.log("Hello World");
+
+// alert("Hey I am practicing bro")
+
+console.log(prompt("Hello Is this Rushikesh"))
+
+// console.log("The value of n is " +nval)
+
+let nval = 123
+
+console.log("The value of n is " +nval)
+
+function addfun(){
+    let a = 10
+    let b = 10
+    console.log(a+b)
+}
+
+console.log("Good morning here are sime steps to take care while define the function I am trying ti view how the js machine will run")
+
+prompt("Are you ready")
+
+addfun()
+*/
+
+/*
+console.log("Hello World");
+
+// alert("Hey I am practicing bro")
+
+console.log(prompt("Hello Is this Rushikesh"))
+
+console.log("The value of n is " +nval)
+
+const con = 123
+
+console.log("The value of n is " +nval)
+
+function addfun(){
+    let a = 10
+    let b = 10
+    console.log(a+b)
+}
+
+console.log("Good morning here are sime steps to take care while define the function I am trying ti view how the js machine will run")
+
+prompt("Are you ready")
+
+addfun()
+
+*/
+
+/*
+// Data Types
+There are 5 Datatypes in javaScript
+1 => Number
+2 => String
+3 => undefined
+4 => Boolean
+5 => 
+
+*/
+
+// var num = 1  //=> number
+// var bool = (1==1)  //=> boolean
+// var str = "Rushikesh" //=> string
+// var unde;
+// var arr = ["Rushikesh", 23, true]
+// for(let i=0; i<3; i++){
+//     console.log(arr[i])
+// }
+// arr[0] = "Khaire"
+// for(let i=0; i<3; i++){
+//     console.log(arr[i])
+// }
+
+// var str1 = "Rushikesh"
+// console.log(str1);
+// str1[0] = '8'
+// console.log(str1);
+
+// document.write(typeof(d))
+
+// FUNCTION //
+
+// function square(){
+//     console.log("Hellow")
+// }
+
+// const car = {
+//     brand : "Mercedece",
+//     model : "Third",
+//     launching : "2019"
+// }
+
+// function changeBrand(obj){
+//     obj.brand = "Rushikesh"
+// }
+
+// console.log(car.brand)
+// changeBrand(car)
+// console.log(car.brand)
+
+// FUNCTION EXPRESSION
+
+const func1 = function (number){
+    return number * number
+}
+
+console.log(func1)
+
